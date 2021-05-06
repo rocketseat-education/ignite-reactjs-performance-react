e8fc518445ca8cd0f50f6bfb2115ef81d522b85a
Diego Fernandes <diego.schell.f@gmail.com>
2021-05-06 09:25:45 -0300

    Bundle Analyzer

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,6 +1,7 @@
 import { memo, useState } from 'react';
 import { AddProductToWishlistProps } from './AddProductToWishlist'
 import dynamic from 'next/dynamic';
+import lodash from 'lodash';
 
 const AddProductToWishlist = dynamic<AddProductToWishlistProps>(() => {
   return import('./AddProductToWishlist').then(mod => mod.AddProductToWishlist)
@@ -37,5 +38,5 @@ function ProductItemComponent({ product, onAddToWishlist }: ProductItemProps) {
 }
 
 export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
-  return Object.is(prevProps.product, nextProps.product)
+  return lodash.isEqual(prevProps.product, nextProps.product)
 })
